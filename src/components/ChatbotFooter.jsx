e752c4c46b1ef7f9f79e7ec0dc9f3cb4f20701b2
Leonard <81501315+l3onard-7@users.noreply.github.com>
2025-10-17 04:45:12 +0100

    Update ChatbotFooter.jsx

--- src/components/ChatbotFooter.jsx
+++ src/components/ChatbotFooter.jsx
@@ -1,35 +1,40 @@
 import React from "react";
 import styles from "../styles/chatfooter.module.css"
 import { useRef } from "react";
+
 function ChatBotFooter({setChatHistory , getModelResponse, chatHistory}) 
 {
    const inputRef = useRef()
-   const handleFromSumnit =(e) =>
-   {
+   
+   const handleFromSumnit = (e) => {
       e.preventDefault();
       const userMessage = inputRef.current.value.trim();
       if(!userMessage) return;
-      inputRef.current.value ="";
+      inputRef.current.value = "";
       getModelResponse(userMessage);
    }
+   
    return(
-      <>
-         <div className={styles.chat_footer}>
-            <form action="#"  
-            onSubmit={handleFromSumnit} 
-            className={styles.chat_form}>
-               <input ref={inputRef} 
+      <div className={styles.chat_footer}>
+         <form action="#"  
+         onSubmit={handleFromSumnit} 
+         className={styles.chat_form}>
+            <input 
+               ref={inputRef} 
                type="text" 
                placeholder="Question?"  
                className={styles.chat_input} 
-               required />
+               required 
+               autoComplete="off"
+            />
             <button 
-            className="material-symbols-rounded">
-            arrow_right
+               type="submit"
+               className="material-symbols-rounded">
+               arrow_right
             </button>
-            </form>
-         </div>
-      </>
+         </form>
+      </div>
    )
 }
+
 export default ChatBotFooter
